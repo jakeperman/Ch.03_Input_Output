6d96098006e52a50034cd11b82d77c3592036bfb
Jake Perman <jakeperman@users.noreply.github.com>
2020-09-20 22:39:48 -0500

    test
    Tesing 123

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -12,7 +12,7 @@ Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 
-
+testing 123...
 
 
 '''
